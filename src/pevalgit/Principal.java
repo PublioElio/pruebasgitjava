7d7a511d2d167a562487d3503e6a82d0889c8231
adria <adria@DESKTOP-K490TVA>
2022-04-05 21:45:43 +0200

    1 Commit: creación proyecto, paquete y clase Principal

---
+++ src/pevalgit/Principal.java
@@ -0,0 +1,18 @@
+package pevalgit;
+
+/**
+ * Clase principal de un ejercicio de la asignatura Entornos de Desarrollo para
+ * probar Github
+ * 
+ * @author Adriano Díaz
+ * @date 05/04/2022
+ *
+ */
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+	}
+
+}
